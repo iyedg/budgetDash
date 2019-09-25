f153ac0bdb62dd599dc98548b03b3ff381cb7dd8
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-09-25 12:00:54 +0100

    extract card function into package

--- budget_dash/layouts.py
+++ budget_dash/layouts.py
@@ -1,33 +1,17 @@
 import dash_core_components as dcc
 import dash_html_components as html
+from .material.card import card
 
-layout1 = html.Div(
-    [
-        html.H3("App 1"),
-        dcc.Dropdown(
-            id="app-1-dropdown",
-            options=[
-                {"label": "App 1 - {}".format(i), "value": i}
-                for i in ["NYC", "MTL", "LA"]
-            ],
-        ),
-        html.Div(id="app-1-display-value"),
-        dcc.Link("Go to App 2", href="/apps/app2"),
-    ]
-)
 
-layout2 = html.Div(
+layout = html.Div(
     [
-        html.H3("App 2"),
-        dcc.Dropdown(
-            id="app-2-dropdown",
-            options=[
-                {"label": "App 2 - {}".format(i), "value": i}
-                for i in ["NYC", "MTL", "LA"]
-            ],
+        html.H3("Hello world"),
+        card(
+            card_title="function generated card",
+            card_content=html.P("This is new"),
+            card_action=[dcc.Link("Hide")],
         ),
-        html.Div(id="app-2-display-value"),
-        dcc.Link("Go to App 1", href="/apps/app1"),
-    ]
+    ],
+    className="container",
 )
 
